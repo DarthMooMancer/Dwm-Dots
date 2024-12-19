b9b88ddac758f392ac8dbfca05692c04997583d4
DarthMooMancer <haynes.a763@outlook.com>
2024-12-19 00:39:47 -0500

    Re-adding Slstatus

---
+++ .config/suckless/slstatus/config.h
@@ -0,0 +1,38 @@
+const unsigned int interval = 1000;
+static const char unknown_str[] = "n/a";
+#define MAXLEN 2048
+
+/*
+ * function            description                     argument (example)
+ *
+ * battery_perc        battery percentage              battery name (BAT0)
+ * battery_remaining   battery remaining HH:MM         battery name (BAT0)
+ * battery_state       battery charging state          battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * cat                 read arbitrary file             path
+ * cpu_freq            cpu frequency in MHz            NULL
+ * cpu_perc            cpu usage in percent            NULL
+ * datetime            date and time                   format string (%F %T)
+ * disk_free           free disk space in GB           mountpoint path (/)
+ * disk_perc           disk usage in percent           mountpoint path (/)
+ * disk_total          total disk space in GB          mountpoint path (/)
+ * disk_used           used disk space in GB           mountpoint path (/)
+ * kernel_release      `uname -r`                      NULL
+ * keyboard_indicators caps/num lock indicators        format string (c?n?)
+ *                                                     see keyboard_indicators.c
+ * ram_free            free memory in GB               NULL
+ * ram_perc            memory usage in percent         NULL
+ * ram_total           total memory size in GB         NULL
+ * ram_used            used memory in GB               NULL
+ * run_command         custom shell command            command (echo foo)
+ * uptime              system uptime                   NULL
+ * username            username of current user        NULL
+ * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
+ */
+
+static const struct arg args[] = {
+  /* function format          argument */
+  { run_command,  " PKGS %s | ", "pacman -Q | wc -l"},
+  { battery_perc, "BAT %s%% | ", "BAT0"},
+  { datetime,     "%s",          "%H:%M %A %d %Y" },
+};
